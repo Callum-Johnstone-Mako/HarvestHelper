b5ee9bc76cd77e65f9680e5a43a2779a6fc9dc9a
Callum <Callum_nzl@live.com>
2023-04-21 17:23:08 +1200

    added a list of different veges to choose from

--- harvesthelper/src/NavBar.js
+++ harvesthelper/src/NavBar.js
@@ -1,31 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const NavContainer = styled.div`
+const Nav = styled.nav`
   display: flex;
-  justify-content: center;
   align-items: center;
-  background-color: #f0f0f0;
-  padding: 16px;
+  justify-content: space-around;
+  background-color: #ccc;
+  padding: 1rem;
 `
 
 const Button = styled.button`
-  margin: 0 8px;
-  padding: 8px 16px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
   cursor: pointer;
 `
 
+const VegetableMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  padding: 0.5rem;
+  background-color: white;
+  border: 1px solid #ccc;
+  z-index: 1;
+`
+
+const vegetables = [
+  { name: 'Carrot', color: 'orange' },
+  { name: 'Lettuce', color: 'green' },
+  { name: 'Beet', color: 'red' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Potato', color: 'saddlebrown' },
+  { name: 'Cucumber', color: 'mediumseagreen' },
+  { name: 'Eggplant', color: 'purple' },
+  { name: 'Radish', color: 'crimson' },
+  { name: 'Pumpkin', color: 'darkorange' },
+  { name: 'Corn', color: 'gold' },
+]
+
 const NavBar = ({ onSelectState }) => {
+  const [showVegetableMenu, setShowVegetableMenu] = useState(false)
+
+  const handlePlantClick = () => {
+    setShowVegetableMenu((prev) => !prev)
+  }
+
   return (
-    <NavContainer>
+    <Nav>
       <Button onClick={() => onSelectState('white')}>Empty</Button>
-      <Button onClick={() => onSelectState('brown')}>Dig Hole</Button>
-      <Button onClick={() => onSelectState('green')}>Plant Vegetable</Button>
-      <Button onClick={() => onSelectState('yellow')}>Harvest Vegetable</Button>
-    </NavContainer>
+      <Button onClick={() => onSelectState('skyblue')}>Dig Hole</Button>
+
+      <div style={{ position: 'relative' }}>
+        <Button onClick={handlePlantClick}>Plant Vegetable</Button>
+        {showVegetableMenu && (
+          <VegetableMenu>
+            {vegetables.map((vegetable) => (
+              <Button
+                key={vegetable.name}
+                onClick={() => {
+                  onSelectState(vegetable.color)
+                  setShowVegetableMenu(false)
+                }}
+                style={{ backgroundColor: vegetable.color }}
+              >
+                {vegetable.name}
+              </Button>
+            ))}
+          </VegetableMenu>
+        )}
+      </div>
+      <Button onClick={() => onSelectState('black')}>Harvest</Button>
+    </Nav>
   )
 }
 
