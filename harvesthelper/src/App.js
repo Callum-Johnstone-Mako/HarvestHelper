9b07eed3f149351869e9fdfbf24cf5fce8e61760
Callum <Callum_nzl@live.com>
2023-04-21 17:12:13 +1200

    added background and changed squares to transparent

--- harvesthelper/src/App.js
+++ harvesthelper/src/App.js
@@ -3,11 +3,20 @@ import Grid from './Grid'
 import NavBar from './NavBar'
 import './App.css'
 
+// Import the background image
+import backgroundImage from './Grass.jpg'
+
 const App = () => {
   const [selectedState, setSelectedState] = useState('white')
 
+  // Add the background image as an inline style for the .App class
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: 'repeat',
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <NavBar onSelectState={setSelectedState} />
       <div style={{ flex: 1, overflow: 'hidden' }}>
         <Grid selectedState={selectedState} />
